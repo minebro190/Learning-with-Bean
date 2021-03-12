5681e29a93fbbc9f46150a00833f4db7ef893f4a
minebro190 <36795818+minebro190@users.noreply.github.com>
2021-03-12 12:50:33 +0800

    Add project files.

---
+++ Learning with Bean/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_with_Bean
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luckyNumbers = { 4, 8, 15, 16, 23, 42};
+
+            string[] friends = new string[5];
+            friends[0] = "Jim";
+            friends[0] = "Kelly";
+
+
+            Console.WriteLine(luckyNumbers[1] );
+
+            Console.ReadLine(); 
+        }
+    }
+}
